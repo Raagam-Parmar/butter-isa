d0712e3ea978904dc517e3e6d8261fe2a8cffd64
raagam <raagam@debian.tech-network.duckdns.org>
2025-12-07 21:30:56 +0530

    Updated specifications.

---
+++ lib/isa.ml
@@ -0,0 +1,23 @@
+type 'a instruction =
+  | Load  of Register.t * Register.t
+  | Store of Register.t * Register.t
+
+  | Lui   of 'a
+  | Lli   of 'a
+
+  | Move  of Register.t * Register.t
+
+  | Add   of Register.t * Register.t
+  | Sub   of Register.t * Register.t
+  | And   of Register.t * Register.t
+  | Or    of Register.t * Register.t
+  | Not   of Register.t
+
+  | Beqz  of Register.t * Register.t
+  | Bltz  of Register.t * Register.t
+  | Jump  of Register.t
+
+  | StPC  of Register.t
+
+  | DPage of Register.t
+  | IPage of Register.t
